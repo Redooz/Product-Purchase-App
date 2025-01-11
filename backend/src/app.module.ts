39210f1a969e9cbedca995556f2474c1f6c769af
Nicolas Olmos <yohannicolas2003@gmail.com>
2025-01-10 19:41:35 -0500

    feat: add database config

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ProductModule } from './modules/product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { typeOrmConfig } from './config/data.source.options';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV || '.env',
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ProductModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ backend/src/config/data.source.options.ts
@@ -0,0 +1,21 @@
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
+
+dotenv.config({ path: '.env' });
+
+export const typeOrmConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [path.join(__dirname, '..') + '/**/*.entity{.ts,.js}'],
+  migrations: [path.join(__dirname, '..') + '/migrations/**/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+  extra: {
+    options: '-c timezone=America/Bogota',
+  },
+};
